9de336e515ed4a280bfb40b6d0677fad137ae548
hkorrizzz <tutvse253@gmail.com>
2024-07-04 21:23:22 +0300

    3

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -3,24 +3,21 @@ package school.mjc.stage0.loops.task5;
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
         if (height % 2 == 0) {
-            System.out.println("The height must be an odd number.");
-            return;
+            height++; // Increase size by 1 if it's even to make it odd
         }
 
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < height; j++) {
-                // Calculate the number of spaces to print
+                // Calculate the number of spaces to print on each side
                 int spaces = Math.abs(height / 2 - i);
-                // Print spaces before the '8's in each row
-                if (j < spaces) {
-                    System.out.print(" ");
-                } else if (j < height - spaces) {
-                    // Print '8's for the hourglass shape
+                if (j >= spaces && j < height - spaces) {
                     System.out.print("8");
+                } else {
+                    System.out.print(" ");
                 }
             }
-            // Move to the next line after printing one row of the hourglass
             System.out.println();
         }
+
     }
 }
