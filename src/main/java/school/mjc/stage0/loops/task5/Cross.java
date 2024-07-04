595cdcdc146d56e64787dac158fe0f4964307923
hkorrizzz <tutvse253@gmail.com>
2024-07-04 21:19:46 +0300

    3

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -2,6 +2,18 @@ package school.mjc.stage0.loops.task5;
 
 public class Cross {
     public void printCross(int sideLength) {
-
+        int middle = sideLength / 2;
+        for (int i = 0; i < sideLength; i++) {
+            for (int j = 0; j < sideLength; j++) {
+                // Print '8' if we're at the middle row or column, otherwise print space
+                if (i == middle || j == middle) {
+                    System.out.print("8");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            // Move to the next line after printing one row of the cross
+            System.out.println();
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,25 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
+        if (height % 2 == 0) {
+            System.out.println("The height must be an odd number.");
+            return;
+        }
 
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < height; j++) {
+                // Calculate the number of spaces to print
+                int spaces = Math.abs(height / 2 - i);
+                // Print spaces before the '8's in each row
+                if (j < spaces) {
+                    System.out.print(" ");
+                } else if (j < height - spaces) {
+                    // Print '8's for the hourglass shape
+                    System.out.print("8");
+                }
+            }
+            // Move to the next line after printing one row of the hourglass
+            System.out.println();
+        }
     }
 }
